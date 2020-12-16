bf6eddb53b9b4a76778564b025ba43a3dec38ebc
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-12-16 14:05:53 +0300

    initial import
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/unistack-org/micro-wrapper-requestid
+
+go 1.15
+
+require (
+	github.com/google/uuid v1.1.2
+	github.com/unistack-org/micro/v3 v3.0.1
+)
---
+++ requestid.go
@@ -0,0 +1,107 @@
+package requestid
+
+import (
+	"context"
+	"net/textproto"
+
+	"github.com/google/uuid"
+	"github.com/unistack-org/micro/v3/client"
+	"github.com/unistack-org/micro/v3/metadata"
+	"github.com/unistack-org/micro/v3/server"
+)
+
+var (
+	// MetadataKey contains metadata key
+	MetadataKey = textproto.CanonicalMIMEHeaderKey("x-request-id")
+)
+
+var (
+	// MetadataFunc wil be used if user not provide own func to fill metadata
+	MetadataFunc = func(ctx context.Context) (context.Context, error) {
+		_, ok := metadata.Get(ctx, MetadataKey)
+		if ok {
+			return ctx, nil
+		}
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return ctx, err
+		}
+		ctx = metadata.Set(ctx, MetadataKey, id.String())
+		return ctx, nil
+	}
+)
+
+type wrapper struct {
+	client.Client
+}
+
+func NewClientWrapper() client.Wrapper {
+	return func(c client.Client) client.Client {
+		handler := &wrapper{
+			Client: c,
+		}
+		return handler
+	}
+}
+
+func NewClientCallWrapper() client.CallWrapper {
+	return func(fn client.CallFunc) client.CallFunc {
+		return func(ctx context.Context, addr string, req client.Request, rsp interface{}, opts client.CallOptions) error {
+			var err error
+			if ctx, err = MetadataFunc(ctx); err != nil {
+				return err
+			}
+			return fn(ctx, addr, req, rsp, opts)
+		}
+	}
+}
+
+func (w *wrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	var err error
+	if ctx, err = MetadataFunc(ctx); err != nil {
+		return err
+	}
+	return w.Client.Call(ctx, req, rsp, opts...)
+}
+
+func (w *wrapper) Stream(ctx context.Context, req client.Request, opts ...client.CallOption) (client.Stream, error) {
+	var err error
+	if ctx, err = MetadataFunc(ctx); err != nil {
+		return nil, err
+	}
+	return w.Client.Stream(ctx, req, opts...)
+}
+
+func (w *wrapper) Publish(ctx context.Context, p client.Message, opts ...client.PublishOption) error {
+	var err error
+	if ctx, err = MetadataFunc(ctx); err != nil {
+		return err
+	}
+	return w.Client.Publish(ctx, p, opts...)
+}
+
+func NewServerHandlerWrapper() server.HandlerWrapper {
+	return func(fn server.HandlerFunc) server.HandlerFunc {
+		return func(ctx context.Context, req server.Request, rsp interface{}) error {
+			var err error
+			if ctx, err = MetadataFunc(ctx); err != nil {
+				return err
+			}
+			return fn(ctx, req, rsp)
+		}
+	}
+}
+
+func NewServerSubscriberWrapper() server.SubscriberWrapper {
+	return func(fn server.SubscriberFunc) server.SubscriberFunc {
+		return func(ctx context.Context, msg server.Message) error {
+			var err error
+			if id, ok := msg.Header()[MetadataKey]; ok {
+				ctx = metadata.Set(ctx, MetadataKey, id)
+			} else if ctx, err = MetadataFunc(ctx); err != nil {
+				return err
+			}
+			return fn(ctx, msg)
+		}
+	}
+}
